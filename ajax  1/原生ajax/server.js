570079fb01e6cfa1af585e4e50d3aa790fe00064
520-forever <a441832620@126.com>
2021-07-14 19:03:32 +0800

    ajax

--- ajax  1/原生ajax/server.js
+++ ajax  1/原生ajax/server.js
@@ -11,6 +11,28 @@ app.get('/server',(request,response)=>{
   //设置响应体
   response.send('HELLO AJAX');
 });
+app.all('/server',(request,response)=>{
+  //设置响应头  设置允许跨域
+  response.setHeader('Access-Control-Allow-Origin','*');
+    //响应头
+    response.setHeader('Access-Control-Allow-Headers','*');
+  //设置响应体
+  response.send('HELLO AJAX');
+});
+app.all('/json-server',(request,response)=>{
+  //设置响应头  设置允许跨域
+  response.setHeader('Access-Control-Allow-Origin','*');
+    //响应头
+    response.setHeader('Access-Control-Allow-Headers','*');
+    //响应一个数据
+    const data={
+      name:'atguigu'
+    };
+    //对对象进行字符串转换
+    let str=JSON.stringify(data);
+  //设置响应体
+  response.send(str);
+});
 //4.监听端口启动服务
 app.listen(8000,()=>{
   console.log("服务已启动，8000端口监听中。。。。");
