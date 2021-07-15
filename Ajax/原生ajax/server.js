df5883de2dc24ad880abe20af7b49ab18d53c101
520-forever <a441832620@126.com>
2021-07-15 15:23:51 +0800

    ajax

--- Ajax/原生ajax/server.js
+++ Ajax/原生ajax/server.js
@@ -40,6 +40,15 @@ app.get('/ie',(request,response)=>{
   //设置响应体
   response.send('HELLO IE121223');
 });
+//延时响应
+app.get('/delay',(request,response)=>{
+  //设置响应头  设置允许跨域
+  response.setHeader('Access-Control-Allow-Origin','*');
+    setTimeout(() => {
+   //设置响应体
+    response.send('延时响应');
+     }, 3000);
+    });
 //4.监听端口启动服务
 app.listen(8000,()=>{
   console.log("服务已启动，8000端口监听中。。。。");
