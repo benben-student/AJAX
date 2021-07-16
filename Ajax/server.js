b8e4b2be132eba3e0eb926c9ee9020ae32767cd1
520-forever <a441832620@126.com>
2021-07-16 17:21:01 +0800

    Ajax完结

--- Ajax/server.js
+++ Ajax/server.js
@@ -125,6 +125,14 @@ app.all('/delay',(request,response)=>{
     //返回结果
     response.end(`${cb}(${str})`);
    });
+   app.all('/cors-server',(request,response)=>{
+     //设置响应头
+     response.setHeader("Access-Control-Allow-Origin","*");
+    // response.setHeader("Access-Control-Allow-Headers","*");
+     //response.setHeader("Access-Control-Allow-Method","*");
+     //response.setHeader("Access-Control-Allow-Origin","http://127.0.0.1:5500");
+     response.send('hello cors');
+   })
 //4.监听端口启动服务
 app.listen(8000,()=>{
   console.log("服务已启动，8000端口监听中。。。。");
